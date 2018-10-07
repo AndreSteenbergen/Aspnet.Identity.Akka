08939eb2c94b34194cd2dfc3187e74ada8a6f0f0
Unknown <andre.steenbergen@gmail.com>
2018-10-07 13:18:37 +0200

    Implemented coordinator Commands

--- ActorHelpers/UserCoordinatorHelper.cs
+++ ActorHelpers/UserCoordinatorHelper.cs
@@ -5,8 +5,10 @@ using Aspnet.Identity.Akka.ActorMessages.UserCoordinator;
 using Aspnet.Identity.Akka.Actors;
 using Aspnet.Identity.Akka.Interfaces;
 using Aspnet.Identity.Akka.Model;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 
 namespace Aspnet.Identity.Akka.ActorHelpers
 {
@@ -26,12 +28,14 @@ namespace Aspnet.Identity.Akka.ActorHelpers
         private Dictionary<TKey, IActorRef> userActors = new Dictionary<TKey, IActorRef>();
 
         //just to find users by key, username, email and external login.
-        private Dictionary<TKey, Tuple<string, string, HashSet<ExternalLogin>>> existingUsers = new Dictionary<TKey, Tuple<string, string, HashSet<ExternalLogin>>>();
         private Dictionary<string, TKey> existingIds = new Dictionary<string, TKey>();
         private Dictionary<string, TKey> existingUserNames = new Dictionary<string, TKey>();
         private Dictionary<string, TKey> existingEmails = new Dictionary<string, TKey>();
         private Dictionary<ExternalLogin, TKey> existingLogins = new Dictionary<ExternalLogin, TKey>();
         private Dictionary<string, Dictionary<string, HashSet<TKey>>> usersByClaims = new Dictionary<string, Dictionary<string, HashSet<TKey>>>();
+
+        //and a reverse lookup
+        private Dictionary<TKey, Tuple<string, string, string, HashSet<ExternalLogin>, HashSet<Claim>>> reverseLookup = new Dictionary<TKey, Tuple<string, string, string, HashSet<ExternalLogin>, HashSet<Claim>>>();
 
         private readonly Func<TKey, IActorRef> createUserActor;
         private readonly Func<IActorContext> getContext;
@@ -85,12 +89,116 @@ namespace Aspnet.Identity.Akka.ActorHelpers
                     FindByUsername(fbu.NormalizedUsername, sender);
                     break;
                 case CreateUser<TKey, TUser> cu:
+                    CreateUser(cu, sender);
                     break;
                 case DeleteUser<TKey> du:
+                    DeleteUser(du, sender);
                     break;
                 case UpdateUser<TKey, TUser> uu:
-                    //check important field; logins email username etc.
+                    UpdateUser(uu, sender);
                     break;
+            }
+        }
+
+        private void UpdateUser(UpdateUser<TKey, TUser> uu, IActorRef sender)
+        {
+            var errors = new List<IdentityError>();
+            foreach (var change in uu.User.Changes)
+            {
+                switch (change)
+                {
+                    case UserNameChanged evt:
+                        {
+                            if (existingUserNames.TryGetValue(evt.UserName.ToUpperInvariant(), out TKey otherId) && !otherId.Equals(uu.Key))
+                            {
+                                errors.Add(new IdentityError { Description = "Duplicate Username found." });
+                            }
+                        }
+                        break;
+                    case NormalizedEmailChanged evt:
+                        {
+                            if (existingEmails.TryGetValue(evt.Email, out TKey otherId) && !otherId.Equals(uu.Key))
+                            {
+                                errors.Add(new IdentityError { Description = "Duplicate Email found." });
+                            }
+                        }
+                        break;
+                    case UserLoginInfoAdded evt:
+                        {
+                            if (existingLogins.TryGetValue(new ExternalLogin(evt.LoginProvider, evt.ProviderKey), out TKey otherId) && !otherId.Equals(uu.Key))
+                            {
+                                errors.Add(new IdentityError { Description = "Duplicate Login found." });
+                            }
+                        }
+                        break;
+                }
+            }
+            if (errors.Count > 0)
+            {
+                sender.Tell(IdentityResult.Failed(errors.ToArray()));
+            }
+            else
+            {
+                if (!userActors.TryGetValue(uu.Key, out IActorRef actor))
+                {
+                    userActors[uu.Key] = actor = createUserActor(uu.Key);
+                }
+                actor.Tell(uu, sender);
+            }
+        }
+
+        private void DeleteUser(DeleteUser<TKey> du, IActorRef sender)
+        {
+            if (userActors.ContainsKey(du.UserId) || existingIds.ContainsKey(du.UserId.ToString()))
+            {
+                if (!userActors.TryGetValue(du.UserId, out IActorRef actor))
+                {
+                    userActors[du.UserId] = actor = createUserActor(du.UserId);
+                }
+                actor.Tell(du, sender);
+            }
+            else
+            {
+                sender.Tell(IdentityResult.Failed());
+            }
+        }
+
+        private void CreateUser(CreateUser<TKey, TUser> cu, IActorRef sender)
+        {
+            var prelim = TestCreateUser(cu);
+            if (prelim.Succeeded)
+            {
+                var actor = userActors[cu.User.Id] = createUserActor(cu.User.Id);
+                actor.Tell(cu, sender);
+            }
+            else
+            {
+                sender.Tell(prelim);
+            }
+        }
+
+        //beware!
+        //race conditions might concur here
+        //maybe lock email/ username somehow, with a timer
+        private IdentityResult TestCreateUser(CreateUser<TKey, TUser> cu)
+        {
+            var usr = cu.User;
+            var errors = new List<IdentityError>();
+            if (existingUserNames.ContainsKey(usr.NormalizedUserName)) errors.Add(new IdentityError { Description = "Username already exists" });
+            if (existingUserNames.ContainsKey(usr.UserName.ToUpperInvariant())) errors.Add(new IdentityError { Description = "Username already exists" });
+            if (existingEmails.ContainsKey(usr.NormalizedEmail)) errors.Add(new IdentityError { Description = "Email already exists" });
+            if (existingIds.ContainsKey(usr.Id.ToString())) errors.Add(new IdentityError { Description = "UserId already present" });
+            foreach (var login in usr.Logins)
+            {
+                if (existingLogins.ContainsKey(new ExternalLogin(login.LoginProvider, login.ProviderKey))) errors.Add(new IdentityError { Description = "External login already present" });
+            }
+
+            if (errors.Count > 0)
+            {
+                return IdentityResult.Failed(errors.ToArray());
+            } else
+            {
+                return IdentityResult.Success;
             }
         }
 
