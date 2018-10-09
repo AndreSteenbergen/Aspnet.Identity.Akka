5a38b522036a2f7802b13d8c7f742b65e166561a
Unknown <andre.steenbergen@gmail.com>
2018-10-09 18:49:01 +0200

    Removed Extension methods already found in aspnet/Identity

--- Aspnet.Identity.Akka/ExtensionMethods/IdentityBuilderExtensions.cs
+++ Aspnet.Identity.Akka/ExtensionMethods/IdentityBuilderExtensions.cs
@@ -9,47 +9,6 @@ namespace Aspnet.Identity.Akka.ExtensionMethods
 {
     public static class IdentityBuilderExtensions
     {
-        public static IdentityBuilder AddIdentity<TUser>(this IServiceCollection services, string defaultSignInScheme)
-            where TUser : class
-        {
-            return services.AddIdentity<TUser>(defaultSignInScheme, setupAction: null);
-        }
-
-        public static IdentityBuilder AddIdentity<TUser>(
-            this IServiceCollection services,
-            string defaultSignInScheme,
-            Action<IdentityOptions> setupAction)
-            where TUser : class
-        {
-            // Services used by identity
-            services.AddAuthentication(options =>
-            {
-                options.DefaultSignInScheme = defaultSignInScheme;
-            });
-
-            // Hosting doesn't add IHttpContextAccessor by default
-            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            // Identity services
-            services.TryAddScoped<IUserValidator<TUser>, UserValidator<TUser>>();
-            services.TryAddScoped<IPasswordValidator<TUser>, PasswordValidator<TUser>>();
-            services.TryAddScoped<IPasswordHasher<TUser>, PasswordHasher<TUser>>();
-            services.TryAddScoped<ILookupNormalizer, UpperInvariantLookupNormalizer>();
-
-            // No interface for the error describer so we can add errors without rev'ing the interface
-            services.TryAddScoped<IdentityErrorDescriber>();
-            services.TryAddScoped<ISecurityStampValidator, SecurityStampValidator<TUser>>();
-            services.TryAddScoped<IUserClaimsPrincipalFactory<TUser>, UserClaimsPrincipalFactory<TUser>>();
-            services.TryAddScoped<UserManager<TUser>, UserManager<TUser>>();
-            services.TryAddScoped<SignInManager<TUser>, SignInManager<TUser>>();
-
-            if (setupAction != null)
-            {
-                services.Configure(setupAction);
-            }
-
-            return new IdentityBuilder(typeof(TUser), services);
-        }
-
         public static IdentityBuilder AddUserStore(this IdentityBuilder builder)
         {
             AddStores(builder.Services, builder.UserType);
@@ -66,7 +25,6 @@ namespace Aspnet.Identity.Akka.ExtensionMethods
 
             var keyType = identityUserType.GenericTypeArguments[0];
             var userStoreType = typeof(UserStore<,>).MakeGenericType(keyType, userType);
-
             services.TryAddScoped(typeof(IUserStore<>).MakeGenericType(userType), userStoreType);
         }
 
